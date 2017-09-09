6de2dcb707b52a217f0d10fda5300c63ef48ec06
Saul Ramos <saul.ramos.2609@gmail.com>
2017-09-09 06:45:11 -0400

    holidays calendar structure

---
+++ assets/js/app.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+//vars
+  var allHolidays = $('.srAllHolidays');
+  var holidayCard = $('srHolidaysList');
+  var dia = $('.srHolidayDay');
+  var mes = $('.srHolidayMonth');
+  var motivo = $('.srHolidayMotivo');
+  var adicional = $('.srHolidayAdd');
+
+
+//get no laborables
+  $.ajax({
+    url: 'http://nolaborables.com.ar/API/v1/actual',
+    dataType: 'json',
+    type: 'GET',
+    success: function(data){
+
+      //Datos a mostrar en la columna izquierda
+      var srJson ='';
+      for( x in data ){
+
+        srJson+='<div id='+'hd-'+x+' class="srHolidaysList srHide">';
+        srJson+='<div class="srHolidayDate">';
+        srJson+='<span class="srHolidayDay">'+data[x].dia+'</span>';
+        srJson+='<span class="srHolidayMonth"> Mes: '+data[x].mes+'</span>';
+        srJson+='</div>';
+        srJson+='<div class="srHolidayDescription">';
+        srJson+='<span class="srHolidayMotivo">'+data[x].motivo+'</span>';
+        srJson+='</div>';
+        srJson+='<span class="srHolidayAdd"><b>Tipo: </b>'+data[x].tipo+'</span>';
+        if(data[x].traslado !=null) {
+        srJson+='<span class="srHolidayAdd"><b>Traslado: </b>'+data[x].traslado+'</span>';
+        }
+        if(data[x].opcional !=null && data[x].religion !=null) {
+          srJson+='<span class="srHolidayAdd"><b>Opcional: </b>'+data[x].opcional+'</span>';
+          srJson+='<span class="srHolidayAdd"><b>Religión: </b>'+data[x].religion+'</span>';
+        }
+        
+        srJson+='</div>';
+      }
+
+      //Armando arreglo
+      var ArrayFeriados = data;
+      var events = [];
+
+      for (var k = 0; k < ArrayFeriados.length; k++) {
+        var mes = "";
+        var dia = "";
+        if(ArrayFeriados[k]["mes"]<10){
+          mes = "0" + ArrayFeriados[k]["mes"];
+        }
+        else{
+          mes = ArrayFeriados[k]["mes"];
+        }
+        if(ArrayFeriados[k]["dia"]<10){
+          dia = "0" + ArrayFeriados[k]["dia"];
+        }
+        else{
+          dia = ArrayFeriados[k]["dia"];
+        }
+        events.push({
+            id: "hd-"+k,
+            title: ArrayFeriados[k]["motivo"],
+            start: (new Date()).getFullYear()+'-'+mes+'-'+dia,
+            color: '#d0ddcc',
+            textColor: '#000'
+        });
+      }
+      allHolidays.append(srJson);
+
+      //init calendar
+      $('#calendar').fullCalendar({
+        eventClick: function(calEvent, jsEvent, view) {
+    
+        $('#'+calEvent.id+'').removeClass('srHide');
+        },
+        header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,listMonth'
+      },
+        events: events
+      });
+    },
+    error: function(data){
+    },
+});
+
+
+
+});
